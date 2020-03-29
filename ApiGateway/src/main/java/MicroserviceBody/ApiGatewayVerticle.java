72473c79e4e64caa488b778583dafe440404d209
Kacper <kacper_dobosz@onet.pl>
2020-03-29 21:27:00 +0200

    Added Circuit braker in ApiGateway

--- ApiGateway/src/main/java/MicroserviceBody/ApiGatewayVerticle.java
+++ ApiGateway/src/main/java/MicroserviceBody/ApiGatewayVerticle.java
@@ -1,12 +1,13 @@
 package MicroserviceBody;
 import io.reactivex.*;
-import io.reactivex.disposables.Disposable;
+import io.vertx.circuitbreaker.CircuitBreakerOptions;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.circuitbreaker.CircuitBreaker;
+import io.vertx.reactivex.circuitbreaker.HystrixMetricHandler;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.buffer.Buffer;
 import io.vertx.reactivex.core.http.HttpClient;
@@ -18,33 +19,43 @@ import io.vertx.reactivex.ext.web.client.WebClient;
 import io.vertx.reactivex.ext.web.codec.BodyCodec;
 import io.vertx.reactivex.ext.web.handler.BodyHandler;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
 public class ApiGatewayVerticle extends AbstractVerticle {
     private String com[]={"/api/customerApi/customers","/api/musicApi/selectArtists"};
-
+    private CircuitBreaker breaker;
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(new ApiGatewayVerticle());
     }
 
     @Override
-    public void start(Future<Void> future) throws Exception{
-        super.start();
+    public void start(Future<Void> future){
+        breaker = CircuitBreaker.create("my-circuit-breaker", vertx,
+                new CircuitBreakerOptions()
+                        .setMaxFailures(5) // number of failure before opening the circuit
+                        .setTimeout(2000) // consider a failure if the operation does not succeed in time
+                        .setFallbackOnFailure(true) // do we call the fallback on failure
+                        .setResetTimeout(10000))
+                .closeHandler(v-> System.out.println("Circuit opened"))
+                .closeHandler(v-> System.out.println("Circuit closed"));
 
         Router router = Router.router(vertx);
 
-        //Body Handler
-        router.route("/*").handler(BodyHandler.create());
+                    router.get("/hystrix-metrics").handler(HystrixMetricHandler.create(vertx));
+                    //Body Handler
+                    router.route("/*").handler(BodyHandler.create());
 
-        // api gateway dispatch
-        router.route("/api/*").handler(this::dispatchOneRequests);
+                    // api gateway dispatch
+                    router.route("/api/*").handler(this::dispatchOneRequests);
 
-        router.route("/both/SelectAll").handler(this::dispatchBothSelectAll);
+                    router.route("/both/selectAll").handler(this::dispatchBothSelectAll);
 
-        // api info
-        router.route("/api").handler(this::Info);
-
+                    // api info
+                    router.route("/api").handler(this::Info);
+                
         // create http server
         vertx.createHttpServer()
                 .requestHandler(router)
@@ -66,31 +77,59 @@ public class ApiGatewayVerticle extends AbstractVerticle {
         if(path.contains("customerApi")){
             doDispatchOne(routingContext,8081,path);
         }else if(path.contains("musicApi")){
-            doDispatchOne(routingContext,8080,path);
+            doDispatchOne(routingContext,8082,path);
         }
     }
 
     private void doDispatchOne(RoutingContext routingContext, int port, String command) {
-        WebClient client = WebClient.create(vertx);
-        if(command.contains("delete")){
-            Single<HttpResponse<Buffer>> delete = client
-                    .delete(port, "localhost", command)
-                    .rxSend();
-            delete.subscribe(resp-> routingContext.response().end("Deleted successfully"));
-        }else if(command.contains("create")){
-            Single<HttpResponse<Buffer>> post = client
-                    .post(port, "localhost", command)
-                    .rxSend();
-            post.subscribe(resp-> routingContext.response().end("Creation successful"));
-        }else {
-            Single <HttpResponse<JsonArray>> get = client
-                    .get(port, "localhost", command)
-                    .as(BodyCodec.jsonArray())
-                    .rxSend();
-            get.subscribe(resp-> routingContext.response().end(resp.body().encodePrettily()));
 
-        }
+        breaker.executeWithFallback(future-> {
+            WebClient client = WebClient.create(vertx);
+            if (command.contains("delete")) {
+                Single<HttpResponse<Buffer>> delete = client
+                        .delete(port, "localhost", command)
+                        .rxSend();
+                delete.subscribe(resp -> {
+                    if(resp.statusCode()!=200){
+                        future.fail("HTTP error");
+                    }else {
+                        routingContext.response().end("Deleted successfully");
+                        future.complete();
+                    }
+                });
+            } else if (command.contains("create")) {
+                Single<HttpResponse<Buffer>> post = client
+                        .post(port, "localhost", command)
+                        .rxSend();
+                post.subscribe(resp -> {
+                    if(resp.statusCode()!=200){
+                        future.fail("HTTP error");
+                    }else {
+                        routingContext.response().end("Creation successful");
+                        future.complete();
+                    }
+                });
+            } else {
+                Single<HttpResponse<JsonArray>> get = client
+                        .get(port, "localhost", command)
+                        .as(BodyCodec.jsonArray())
+                        .rxSend();
+                get.subscribe(resp -> {
+                    if(resp.statusCode()!=200){
+                        future.fail("HTTP error");
+                    }else {
+                        routingContext.response().end(resp.body().encodePrettily());
+                        future.complete();
+                    }
+                });
+            }
+        },v->{
+                return "Test1";
+            },ar->{
+                System.out.println("Result: " + ar.result());
+            });
     }
+
 
 
     private void Info(RoutingContext routingContext) {
@@ -106,13 +145,15 @@ public class ApiGatewayVerticle extends AbstractVerticle {
 
 
     public void dispatchBothSelectAll(RoutingContext context){
+        AtomicBoolean port_8081= new AtomicBoolean(false);
+        boolean port_8082=false;
         HttpClient client = vertx.createHttpClient();
 
-        HttpClientRequest req1 = client.request(HttpMethod.GET, 8080, "localhost", com[0]);
-        HttpClientRequest req2 = client.request(HttpMethod.GET, 8081, "localhost", com[1]);
-
+        HttpClientRequest req1 = client.request(HttpMethod.GET, 8081, "localhost", com[0]);
         Flowable<JsonArray> obs1 = req1.toFlowable().flatMap(element->element.toFlowable()).
                 map(buffer -> new JsonArray(buffer.toString("UTF-8")));
+
+        HttpClientRequest req2 = client.request(HttpMethod.GET, 8082, "localhost", com[1]);
         Flowable<JsonArray> obs2 = req2.toFlowable().flatMap(element->element.toFlowable()).
                 map(buffer -> new JsonArray(buffer.toString("UTF-8")));
 
